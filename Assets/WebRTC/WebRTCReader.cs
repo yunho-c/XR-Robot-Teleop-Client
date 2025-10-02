ca3ea435921091a22ff25d59fc71e2ad7377b801
Sundhararajan Vinodh Sangeetha <sundharvs@gmail.com>
2025-10-01 22:18:03 -0400

    Add WHEP WebRTC video stream receiver

---
+++ Assets/WebRTC/WebRTCReader.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using UnityEngine;
+using Unity.WebRTC;
+
+public class WebRTCReader : MonoBehaviour
+{
+    public string url = "http://localhost:8889/mystream/whep";
+
+    private RTCPeerConnection pc;
+    private MediaStream receiveStream;
+	private Renderer targetRenderer; //meshRenderer
+
+    void Start()
+    {
+		targetRenderer = GetComponent<Renderer>();
+        pc = new RTCPeerConnection();
+        receiveStream = new MediaStream();
+
+        pc.OnTrack = e =>
+        {
+            receiveStream.AddTrack(e.Track);
+        };
+
+        receiveStream.OnAddTrack = e =>
+        {
+            if (e.Track is VideoStreamTrack videoTrack)
+            {
+                videoTrack.OnVideoReceived += (tex) =>
+                {
+                    targetRenderer.material.mainTexture = tex;
+                };
+            }
+        };
+
+        RTCRtpTransceiverInit init = new RTCRtpTransceiverInit();
+        init.direction = RTCRtpTransceiverDirection.RecvOnly;
+        pc.AddTransceiver(TrackKind.Audio, init);
+        pc.AddTransceiver(TrackKind.Video, init);
+
+        StartCoroutine(WebRTC.Update());
+        StartCoroutine(createOffer());
+    }
+
+    private IEnumerator createOffer()
+    {
+        var op = pc.CreateOffer();
+        yield return op;
+        if (op.IsError) {
+            Debug.LogError("CreateOffer() failed");
+            yield break;
+        }
+
+        yield return setLocalDescription(op.Desc);
+    }
+
+    private IEnumerator setLocalDescription(RTCSessionDescription offer)
+    {
+        var op = pc.SetLocalDescription(ref offer);
+        yield return op;
+        if (op.IsError) {
+            Debug.LogError("SetLocalDescription() failed");
+            yield break;
+        }
+
+        yield return postOffer(offer);
+    }
+
+    private IEnumerator postOffer(RTCSessionDescription offer)
+    {
+        var content = new System.Net.Http.StringContent(offer.sdp);
+        content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/sdp");
+        var client = new System.Net.Http.HttpClient();
+
+        var task = System.Threading.Tasks.Task.Run(async () => {
+            var res = await client.PostAsync(new System.UriBuilder(url).Uri, content);
+            res.EnsureSuccessStatusCode();
+            return await res.Content.ReadAsStringAsync();
+        });
+        yield return new WaitUntil(() => task.IsCompleted);
+        if (task.Exception != null) {
+            Debug.LogError(task.Exception);
+            yield break;
+        }
+
+        yield return setRemoteDescription(task.Result);
+    }
+
+    private IEnumerator setRemoteDescription(string answer)
+    {
+        RTCSessionDescription desc = new RTCSessionDescription();
+        desc.type = RTCSdpType.Answer;
+        desc.sdp = answer;
+        var op = pc.SetRemoteDescription(ref desc);
+        yield return op;
+        if (op.IsError) {
+            Debug.LogError("SetRemoteDescription() failed");
+            yield break;
+        }
+
+        yield break;
+    }
+
+    void OnDestroy()
+    {
+        pc?.Close();
+        pc?.Dispose();
+        receiveStream?.Dispose();
+    }
+}
